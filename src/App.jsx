fbd1e4012afd78fd7e559354d9e33a3b77d3d390
sakit333 <sak528264@gmail.com>
2025-03-05 14:55:50 +0530

    done with hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -9,7 +9,7 @@ import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-    <Router basename="/sakdevops">  {/* Add basename */}
+    <Router> {/* Remove basename */}
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="/sakdevops/">My Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#/">My Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/sakdevops/">Home</Nav.Link>  {/* Fix Home link */}
-            <Nav.Link href="/sakdevops/about">About</Nav.Link>
-            <Nav.Link href="/sakdevops/projects">Projects</Nav.Link>
-            <Nav.Link href="/sakdevops/contact">Contact</Nav.Link>
+            <Nav.Link href="#/">Home</Nav.Link>  {/* Fix Home link */}
+            <Nav.Link href="#/about">About</Nav.Link>
+            <Nav.Link href="#/projects">Projects</Nav.Link>
+            <Nav.Link href="#/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
